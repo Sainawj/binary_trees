cdf42ab18d2182b7ba26dcb2be6e2f9174e76d55
Jonathan Saina <sainajonathan@gmail.com>
2024-06-04 07:27:55 -0700

    8th commit.3

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -15,7 +15,7 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 
 /* Check if the node has no children */
 	if (node->left == NULL && node->right == NULL)
-		return (1); 
+		return (1);
 
-	return (0); 
+	return (0);
 }
