9ac5bade3db6b9fbe9429a9209de424891b54377
Jonathan Saina <sainajonathan@gmail.com>
2024-06-06 03:07:59 -0700

    25th commit.1

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -33,7 +33,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
 }
 /**
- * linked_node - this function makes a linked list from depth level and node
+ * linked_node - function that makes a linked list from depth level and node
  * @head: pointer to head of linked list
  * @tree: node to store
  * @level: depth of node to store
@@ -68,7 +68,6 @@ void linked_node(link_t **head, const binary_tree_t *tree, size_t level)
 }
 /**
  * recursion - goes through the complete tree and each stores each node
- * in linked_node function
  * @head: pointer to head of linked list
  * @tree: node to check
  * Return: Nothing by default it affects the pointer
